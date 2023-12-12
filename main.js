0972d8ab4990e6703b0786ed1c231d2c0230e742
petxmr <111649405+petxmr@users.noreply.github.com>
2023-12-12 21:54:41 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const Lexer = require('./lib/Lexer');
 const Parser = require('./lib/Parser');
 const Evaluator = require('./lib/Evaluator');
 
-const input = '1 + 2 * 3';
+const input = 'int a = 5 + 5; a';
 
 const lexer = new Lexer(input);
 const tokens = lexer.lex();
@@ -17,4 +17,4 @@ console.log("AST: ", ast);
 const evaluator = new Evaluator(ast);
 const result = evaluator.evaluate();
 
-console.log('Result:', result);
+console.log('Result:', result.evaluate());
